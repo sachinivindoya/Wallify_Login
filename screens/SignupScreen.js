d2055ec4460c353f8bbf7d35cebe1b5daae39034
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2024-01-28 19:35:30 +0530

    signup View testing

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import { View, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native'
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { useNavigation } from '@react-navigation/native';
+import Animated, { FadeIn, FadeInDown, FadeInUp } from 'react-native-reanimated';
+import { View, Text, Image, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
 
 function SignupScreen() {
     const navigation = useNavigation();
   return (
-    <View>
-        <Text>SignupScreen</Text>
+    <View className="bg-white h-full w-full">
+        <StatusBar style="light" />
+       
     </View>
     
   )
